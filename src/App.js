ac12c44cd834a87a019acc89b904e27a8dffd2a3
Piyush Rai <110000004+Peeyusj@users.noreply.github.com>
2023-05-03 09:37:22 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Banner from "./components/Banner";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import "./App.css"
+import Banner2 from "./components/Banner2";
 function App() {
  
 
@@ -9,8 +10,9 @@ function App() {
     <div>
       <Navbar/>
       <Home/>
+      {/* data using strapi */}
       <Banner/>
-
+      <Banner2/>
       
     </div>
   );
